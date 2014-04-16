48e03b92cd6a64246247b0f894480989514d67be
ael-code <tommy.ael@gmail.com>
2014-04-16 18:34:28 +0200

    removed compile.sh

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-options="-I lib -O3 -g -Wall -D_GNU_SOURCE " #-D DEBUG"
-
-echo "compiling client"
-gcc -o bin/client.o client/client.c lib/seats.c lib/conversion.c $options
-
-echo "compiling server"
-gcc -o bin/server.o server/server.c server/file_op.c server/threads.c server/reservation.c server/chiavazione.c server/matrix.c lib/seats.c lib/conversion.c -pthread $options
